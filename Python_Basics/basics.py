5d2d7745331e55782d229dea4a5cbf484db99cda
Siddhant128-bit <80937266+Siddhant128-bit@users.noreply.github.com>
2024-02-23 20:18:58 +0545

    Basics Setup

---
+++ Python_Basics/basics.py
@@ -0,0 +1,10 @@
+
+# created by Siddhant Sharma (inline comment)
+'''
+Created by Siddhant Sharma for Lecture of Python (multiple line comments)
+Online.
+Very Basics task
+'''
+
+# Learning about print statement
+print("Hello world")
---
+++ Python_Basics/calculations.py
@@ -0,0 +1,23 @@
+'''
+This is a simple app 
+where we do calculations
+between 2 numbers
+'''
+
+#6 plus 4 calculation
+print('sum: ')
+print(4+6)
+
+# 6 minus 4 calculation
+print('difference: ')
+print(6-4)
+
+#multiply 5 and 23
+print('Multiplication: ')
+print(5*23)
+
+#Division of 25 by 5
+print("Divions: ")
+print(23/5)
+
+
