a2fe296def1c767037ca68687099425b4aa57c28
Anu <anuu.sapkota@gmail.com>
2024-03-03 13:51:54 +0545

    Question 2: Vowel counter function

--- Assignments/string_manipulations.py
+++ Assignments/string_manipulations.py
@@ -29,21 +29,40 @@ def reverseStringApproach2 (str):
 
 reverseStringApproach2(u_input2)
 
+#Question2:
 '''
 Count Vowels in a String: Write a function that takes a string as input and returns the number of vowels (a, e, i, o, u) in the string. 
 For example, if the input is "hello", the output should be 2.
 using loop
+'''
+u_input3 = input('Enter your text: ').lower()
 
+def vowelCounter(any):
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  NumberOfVowels = 0
+  for vowel in vowels:
+    if vowel in any:
+      NumberOfVowels+= any.count(vowel)
+  
+  print(f'Number Of Vowels: {NumberOfVowels}')
+
+vowelCounter(u_input3)
+
+
+'''
 Check Palindrome: Write a function that checks if a given string is a palindrome. 
 A palindrome is a word, phrase, number, or other sequences of characters that reads the same forward and backward. 
 For example, "radar" is a palindrome.
+'''
 
+'''
 String Anagrams: Write a function that checks if two strings are anagrams of each other. 
 An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
 typically using all the original letters exactly once. 
 For example, "listen" and "silent" are anagrams.
+'''
 
-
+'''
 #Optional Try your best
 Longest Substring Without Repeating Characters: Given a string, find the length of the longest substring without repeating characters. 
 For example, the longest substring without repeating characters in "abcabcbb" is "abc", 
